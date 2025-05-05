9d809c4df81cfab9c9205cd8aecd63ad68e6b4f7
Shaon Majumder <smazoomder@gmail.com>
2025-05-05 20:25:23 +0600

    Migrations successfully run

---
+++ database/migrations/2025_05_05_140039_create_employee_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_details', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('employee_id');
+            $table->string('designation');
+            $table->float('salary');
+            $table->text('address');
+            $table->date('joined_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_details');
+    }
+}
